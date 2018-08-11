5ad88685dbd1a2bd7ce6a07bcb2497d63a8f9f81
Rafael Rollo <rafaelrollo92@gmail.com>
2018-08-11 14:32:21 -0300

    Usando DTO de output

--- src/main/java/br/com/alura/forum/controller/TopicController.java
+++ src/main/java/br/com/alura/forum/controller/TopicController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import br.com.alura.forum.controller.dto.output.TopicBriefOutputDto;
 import br.com.alura.forum.model.Category;
 import br.com.alura.forum.model.Course;
 import br.com.alura.forum.model.Subcategory;
@@ -20,11 +21,12 @@ public class TopicController {
 
 	@ResponseBody
 	@RequestMapping("/api/topics")
-	public List<Topic> listTopics() {
+	public List<TopicBriefOutputDto> listTopics() {
 	
 		Course course = new Course("Java e JSF", new Subcategory("Java", new Category("Programação")));
 		Topic topic = new Topic("Problemas com o JSF", new User("Fulano"), course);
 		
-		return Arrays.asList(topic, topic, topic);
+		List<Topic> topics = Arrays.asList(topic, topic, topic);
+		return TopicBriefOutputDto.listFromThe(topics);
 	}
 }
---
+++ src/main/java/br/com/alura/forum/controller/dto/output/TopicBriefOutputDto.java
@@ -0,0 +1,69 @@
+package br.com.alura.forum.controller.dto.output;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import br.com.alura.forum.model.Topic;
+
+public class TopicBriefOutputDto {
+
+	private String shortDescription;
+	private long secondsSinceLastUpdate;
+	private String ownerName;
+	private String courseName;
+	private String subcategoryName;
+	private String categoryName;
+	private int numberOfResponses;
+	
+	public TopicBriefOutputDto(Topic topic) {
+		this.shortDescription = topic.getShortDescription();
+		this.secondsSinceLastUpdate = getSecondsSince(topic.getLastUpdate());
+		this.ownerName = topic.getOwner().getName();
+		this.courseName = topic.getCourse().getName();
+		this.subcategoryName = topic.getCourse().getSubcategoryName();
+		this.categoryName = topic.getCourse().getCategoryName();
+		this.numberOfResponses = topic.getAnswers().size();
+	}
+
+	private long getSecondsSince(Instant lastUpdate) {
+		return Duration.between(lastUpdate, Instant.now())
+				.get(ChronoUnit.SECONDS);
+	}
+
+	public String getShortDescription() {
+		return shortDescription;
+	}
+
+	public long getSecondsSinceLastUpdate() {
+		return secondsSinceLastUpdate;
+	}
+
+	public String getOwnerName() {
+		return ownerName;
+	}
+
+	public String getCourseName() {
+		return courseName;
+	}
+
+	public String getSubcategoryName() {
+		return subcategoryName;
+	}
+
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+	public int getNumberOfResponses() {
+		return numberOfResponses;
+	}
+
+	public static List<TopicBriefOutputDto> listFromThe(List<Topic> topics) {
+		return topics.stream()
+				.map(TopicBriefOutputDto::new)
+				.collect(Collectors.toList());
+	} 
+}
