78f6d45dc86d47222dfa6df835ed94dd5cea2d9e
Rafael Rollo <rafaelrollo92@gmail.com>
2018-09-10 12:40:24 -0300

    Personalizando response messages

--- src/main/java/br/com/alura/forum/configuration/SwaggerConfiguration.java
+++ src/main/java/br/com/alura/forum/configuration/SwaggerConfiguration.java
@@ -1,11 +1,15 @@
 package br.com.alura.forum.configuration;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
@@ -18,14 +22,28 @@ public class SwaggerConfiguration {
 
 	@Bean
     public Docket api() { 
-        return new Docket(DocumentationType.SWAGGER_2)  
-          .select()                                  
-          .apis(RequestHandlerSelectors.basePackage("br.com.alura.forum"))              
-          .paths(PathSelectors.ant("/api/*"))                          
-          .build()
-          .apiInfo(apiInfo());                                           
+        return new Docket(DocumentationType.SWAGGER_2)
+        		.select()
+        		.apis(RequestHandlerSelectors.basePackage("br.com.alura.forum"))
+        		.paths(PathSelectors.ant("/api/*"))
+        		.build()
+        		.apiInfo(apiInfo())
+        		.globalResponseMessage(RequestMethod.GET, 
+        			Arrays.asList(
+        				new ResponseMessageBuilder()
+        					.code(500)
+        					.message("Xii! Deu erro interno no servidor.")
+        					.build(),
+        				new ResponseMessageBuilder()
+        					.code(403)
+        					.message("Forbidden! Você não pode acessar esse recurso.")
+        					.build(),
+        				new ResponseMessageBuilder()
+        					.code(404)
+        					.message("O recurso que você buscou não foi encontrado.")
+        					.build()));                                           
     }
-
+	
 	private ApiInfo apiInfo() {
 		Contact contato = new Contact("Alura", 
 				"https://cursos.alura.com.br/", "contato@alura.com.br");
