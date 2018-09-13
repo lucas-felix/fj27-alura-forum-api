b0fea8e9c1ade6d2aa5e23f4c5eacc199c85e675
Rafael Rollo <rafaelrollo92@gmail.com>
2018-09-13 17:45:05 -0300

    Implementado o código mínimo necessário para autenticação
    Classes de UsersService, User e Role

--- src/main/java/br/com/alura/forum/security/configuration/SecurityConfiguration.java
+++ src/main/java/br/com/alura/forum/security/configuration/SecurityConfiguration.java
@@ -1,20 +1,33 @@
 package br.com.alura.forum.security.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter  {
 
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-			.antMatchers("/api/topics/**").permitAll()
-			.anyRequest().authenticated()
-		.and()
-			.formLogin();
+				.antMatchers("/api/topics/**").permitAll()
+				.anyRequest().authenticated()
+			.and()
+				.formLogin();
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService)
+			.passwordEncoder(new BCryptPasswordEncoder());
 	}
 }
