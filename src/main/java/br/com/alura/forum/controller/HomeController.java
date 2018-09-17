82385eaf52a98e21d875c4c3f25fe83cc536d34e
Rafael Rollo <rafaelrollo92@gmail.com>
2018-09-17 18:25:33 -0300

    Autenticando requisições através da validação do JWT

--- src/main/java/br/com/alura/forum/controller/HomeController.java
+++ src/main/java/br/com/alura/forum/controller/HomeController.java
@@ -1,9 +1,12 @@
 package br.com.alura.forum.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import br.com.alura.forum.model.User;
 
 @Controller
 @CrossOrigin
@@ -11,8 +14,8 @@ public class HomeController {
 
     @RequestMapping("/")
     @ResponseBody
-    public String index() {
+    public String index(@AuthenticationPrincipal User user) {
         System.out.println("Hello World!");
-        return "Hello World com Spring Boot e MVC";
+        return "Hello World com Spring Boot e MVC, " + user.getName();
     }
 }
