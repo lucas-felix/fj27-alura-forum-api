9ecda0ab64d7a7834e921c005b998f2471b1319d
Rafael Rollo <rafaelrollo92@gmail.com>
2018-09-17 18:25:33 -0300

    Autenticando requisições através da validação do JWT

--- src/main/java/br/com/alura/forum/security/configuration/SecurityConfiguration.java
+++ src/main/java/br/com/alura/forum/security/configuration/SecurityConfiguration.java
@@ -1,5 +1,13 @@
 package br.com.alura.forum.security.configuration;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,16 +19,25 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import br.com.alura.forum.security.JwtAuthenticationFilter;
+import br.com.alura.forum.security.jwt.TokenManager;
+import br.com.alura.forum.security.service.UsersService;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter  {
 
 	@Autowired
-	private UserDetailsService userDetailsService;
+	private UsersService usersService;
 
+	@Autowired
+	private TokenManager tokenManager;
+	
 	@Override
 	@Bean(BeanIds.AUTHENTICATION_MANAGER)
 	public AuthenticationManager authenticationManagerBean() throws Exception {
@@ -36,13 +53,18 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter  {
 			.and()
 				.csrf().disable()
 			.sessionManagement()
-				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and()
+				.addFilterBefore(new JwtAuthenticationFilter(tokenManager, usersService), 
+						UsernamePasswordAuthenticationFilter.class)
+			.exceptionHandling()
+				.authenticationEntryPoint(new JwtAuthenticationEntryPoint());
 				
 	}
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userDetailsService)
+		auth.userDetailsService(this.usersService)
 			.passwordEncoder(new BCryptPasswordEncoder());
 	}
 	
@@ -50,5 +72,18 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter  {
 	public void configure(WebSecurity web) throws Exception {
 		web.ignoring().antMatchers("/**.html",  "/v2/api-docs", "/webjars/**", 
 				"/configuration/**", "/swagger-resources/**");
+	}
+	
+	private static class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+		private static final Logger logger = LoggerFactory.getLogger(JwtAuthenticationEntryPoint.class);
+		
+		@Override
+		public void commence(HttpServletRequest request, HttpServletResponse response,
+				AuthenticationException authException) throws IOException, ServletException {
+			
+			logger.error("Um acesso não autorizado foi verificado. Mensagem: {}", authException.getMessage());
+			response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Você não está autorizado a acessar esse recurso.");
+		}
 	}
 }
