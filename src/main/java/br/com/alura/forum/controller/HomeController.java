4f8c8c0592a7d6fb979ff209be5d7bae3da89fb5
Lucas Felix <lfelixsampaio@gmail.com>
2019-09-28 08:39:55 -0300

    adding integration test for HomeController class

--- src/main/java/br/com/alura/forum/controller/HomeController.java
+++ src/main/java/br/com/alura/forum/controller/HomeController.java
@@ -1,16 +1,19 @@
 package br.com.alura.forum.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class HomeController {
+class HomeController {
 
-    @RequestMapping("/")
-    @ResponseBody
-    public String index() {
-        System.out.println("Hello World!");
+    private static final Logger log = LoggerFactory.getLogger(HomeController.class);
+
+    @GetMapping("/")
+    @ResponseBody String index() {
+        log.info("Hello World!");
         return "Hello World com Spring Boot e MVC";
     }
 }
---
+++ src/test/java/br/com/alura/forum/controller/HomeControllerTest.java
@@ -0,0 +1,27 @@
+package br.com.alura.forum.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(HomeController.class)
+class HomeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void shouldReturnHelloWorldMessage() throws Exception {
+        mockMvc.perform(get("/"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello World com Spring Boot e MVC")));
+    }
+}
